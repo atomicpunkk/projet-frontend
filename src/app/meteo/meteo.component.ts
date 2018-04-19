101a05189d2bbdb6df1f9ddaf68cccb63c366a2b
yaman0 <yamanO@protonmail.com>
2018-04-19 17:51:18 +0200

    Create meteo component

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Town } from '../model/town/town'
+import { TownService } from '../model/town/town.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-meteo',
   templateUrl: './meteo.component.html',
-  styleUrls: ['./meteo.component.css']
+  styleUrls: ['./meteo.component.css'],
 })
 export class MeteoComponent implements OnInit {
-
-  constructor() { }
+  town: Town;
+  private townService: TownService;
+  constructor(private http: HttpClient) {
+	  this.townService = new TownService(http)
+  }
 
   ngOnInit() {
+    this.townService.getTown(1).subscribe(town => {
+      this.town=town;
+    });
   }
 
 }
